0572826925cbe8e18815a3b05a35686a98682500
HUMBERTO MARTINS <humbertosant.mart@gmail.com>
2020-12-21 15:08:41 -0300

    Tabela Instrutores - Aula 16

--- app/Http/Controllers/CadInstrutoresController.php
+++ app/Http/Controllers/CadInstrutoresController.php
@@ -6,5 +6,8 @@ use Illuminate\Http\Request;
 
 class CadInstrutoresController extends Controller
 {
-    //
+    public function index(){
+        $inst = instrutor::orderby('id', 'desc')->paginate();
+        return view('instrutores.index', ['inst' => $inst]);
+    }
 }
